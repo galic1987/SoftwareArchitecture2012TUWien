7b4b7b1566d307b6d5ba0a707dfd1ca59892ad14
unknown <vbenus@VBENU-TOSH.evolit.co.at>
2012-12-09 01:10:50 +0100

    skeleton

---
+++ eclipse/client/src/at/ac/tuwien/software/architectures/ws2012/client/Main.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.software.architectures.ws2012.client;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
