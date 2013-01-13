ce7e773579d39f974a2e655057303a99a900bd64
Milos Sipetic <msipetic@gmail.com>
2013-01-13 13:28:32 +0100

    Protocol buffers compiler added Message definitions added

---
+++ Messages/generate.bat
@@ -0,0 +1,3 @@
+..\Tools\protobuf\protoc.exe --java_out=".\java" --python_out=".\Python" General.proto
+..\Tools\protobuf\protoc.exe --java_out=".\java" --python_out=".\Python" server.proto
+..\Tools\protobuf\protoc.exe --java_out=".\java" --python_out=".\Python" peer.proto
