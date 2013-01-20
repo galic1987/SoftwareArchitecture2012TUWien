b7bfecdedd2ed6070980a4c660b1bb9543840125
Milos Sipetic <msipetic@gmail.com>
2013-01-20 23:30:26 +0100

    Fixed some crap with fingeprint util

--- eclipse/Common/FingerprintUtil.java
+++ eclipse/Common/FingerprintUtil.java
@@ -1,8 +1,18 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import ac.at.tuwien.infosys.swa.audio.Fingerprint;
+
+public class FingerprintUtil {
+	
 	public static byte[] serializeFingerprint(Fingerprint obj) throws IOException {
-        ByteArrayOutputStream b = new ByteArrayOutputStream();
-        ObjectOutputStream o = new ObjectOutputStream(b);
-        o.writeObject(obj);
-        return b.toByteArray();
+	        ByteArrayOutputStream b = new ByteArrayOutputStream();
+	        ObjectOutputStream o = new ObjectOutputStream(b);
+	        o.writeObject(obj);
+	        return b.toByteArray();
     }
 
     public static Fingerprint deserializeFingerprint(byte[] bytes) throws IOException, ClassNotFoundException {
@@ -10,3 +20,4 @@
         ObjectInputStream o = new ObjectInputStream(b);
         return (Fingerprint) o.readObject();
     }
+}
